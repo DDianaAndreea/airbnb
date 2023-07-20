1dc9db88e20dfb29e77047175bbef04a166357d0
DDianaAndreea <dyusk_96@yahoo.com>
2023-07-20 15:49:13 +0300

    add info card component

--- app/search/page.js
+++ app/search/page.js
@@ -1,35 +1,45 @@
 'use client'
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import InfoCard from "@/components/InfoCard";
 import { usePathname, useSearchParams} from "next/navigation";
+import { cardsInfo } from "./cardsInfo";
 
-export default function Search() {
- const path = usePathname();
- const searchParam= useSearchParams();
 
-  let paramsString=`{`
-  let urlString=searchParam.toString();
-  urlString = urlString.split('&');
-  urlString.forEach(element => {
-    const newElement=element.split('=');
-    paramsString=[paramsString,`"`,newElement[0],`"`,`:`,`"`,newElement[1],`"`,`,`].join("")
-  });
- const resultString=paramsString.slice(0,-1)+'}';
+export default function Search({searchParams}) {
+//  const path = usePathname();
+//  const searchParam= useSearchParams();
 
- const{location,startDate,endDate,numberOfGuests}=JSON.parse(resultString);
+//   let paramsString=`{`
+//   let urlString=searchParam.toString();
+//   urlString = urlString.split('&');
+//   urlString.forEach(element => {
+//     const newElement=element.split('=');
+//     paramsString=[paramsString,`"`,newElement[0],`"`,`:`,`"`,newElement[1],`"`,`,`].join("")
+//   });
+//  const resultString=paramsString.slice(0,-1)+'}';
+
+//  const{location,startDate,endDate,numberOfGuests}=JSON.parse(resultString);
 
 //  console.log(path);
 //  console.log(location,startDate,endDate,numberOfGuests);
 
-  const formatedStartDate = new Date(startDate.slice(0,-18)).toDateString();
-  const formatedEndDate = new Date(endDate.slice(0,-18)).toDateString();
+//   const formatedStartDate = new Date(startDate.slice(0,-18)).toDateString();
+//   const formatedEndDate = new Date(endDate.slice(0,-18)).toDateString();
+
+//   const range = `${formatedStartDate}-${formatedEndDate}`;
+
+  const{location,startDate,endDate,numberOfGuests}=searchParams;
+
+  const formatedStartDate = new Date(startDate).toDateString();
+  const formatedEndDate = new Date(endDate).toDateString();
 
   const range = `${formatedStartDate}-${formatedEndDate}`;
 
   return (
     <div >
         <Header placeholder={`${location} | ${range} | ${numberOfGuests}` }/>
-        <main className="flex">
+        <main className="flex flex-col px-3">
           <section>
             <p className="text-xs my-3"> 300+ Stays -{range}- for {numberOfGuests} guests </p>
             <h1 className="text-3xl font-semibold mt-2 mb-6 ">Stays in {location}</h1>
@@ -42,9 +52,42 @@ export default function Search() {
             </div>
           
           </section>
+
+          <div className="flex flex-col">
+          { cardsInfo.map(({img,location,title,description,star,price,total})=>(
+            <InfoCard 
+              key={img}
+              img={img}
+              location={location}
+              title={title}
+              description={description}
+              star={star}
+              price={price}
+              total={total}
+            />
+            ))
+          }
+          </div>
+      
+
         </main>
 
         <Footer />
     </div>
   )
 }
+
+// export async function getServerSideProps() {
+
+//   const cardsInfo = await fetch('https://links.papareact.com/isz')
+//   .then((res)=> res.json())
+  
+//   return { 
+//     props:{
+//       cardsInfo,
+//   }}
+// }
+
+
+
+
